0d9dac6bb5d9de3b874ea72a899cc87820bfcc54
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-08-30 19:35:27 +0200

    Makes a video-js component

--- webapp/src/app/video-js/video-js.component.ts
+++ webapp/src/app/video-js/video-js.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+
+import videojs from 'video.js';
 
 @Component({
-  selector: 'app-video-js',
-  templateUrl: './video-js.component.html',
-  styleUrls: ['./video-js.component.css']
+	selector: 'app-video-js',
+	templateUrl: './video-js.component.html',
+	styleUrls: ['./video-js.component.css']
 })
-export class VideoJsComponent implements OnInit {
+export class VideoJsComponent implements OnInit,OnChanges {
+	public vjs: videojs.Player;
+	@Input() urlVideo: string;
+	@Input() urlPoster: string;
+	constructor() {	}
 
-  constructor() { }
+	ngOnInit() {
+	}
 
-  ngOnInit() {
-  }
-
+	ngOnChanges() {
+		if ( this.urlVideo.length == 0 ) {
+			return;
+		}
+		const options = {
+			'sources' : [{
+				'src' : this.urlVideo,
+				'type' : 'application/x-mpegURL'
+			}
+						],
+			'poster' : this.urlPoster
+		};
+		this.vjs = videojs('my-player', options);
+	}
 }
