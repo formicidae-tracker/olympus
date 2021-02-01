024b02a5a33a38019d7967c6645fe055adcd2b8e
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-02-01 18:39:29 +0100

    [wip]

--- webapp/src/app/zone-preview/zone-preview.component.spec.ts
+++ webapp/src/app/zone-preview/zone-preview.component.spec.ts
@@ -6,7 +6,6 @@ import { ZonePreviewComponent } from './zone-preview.component';
 describe('ZonePreviewComponent', () => {
 	let component: ZonePreviewComponent;
 	let fixture: ComponentFixture<ZonePreviewComponent>;
-	let summary = new ZoneSummaryReport('somehost','somezone');
 
 	beforeEach(waitForAsync(() => {
 		TestBed.configureTestingModule({
@@ -16,11 +15,9 @@ describe('ZonePreviewComponent', () => {
 	}));
 
 
-
 	beforeEach(() => {
 		fixture = TestBed.createComponent(ZonePreviewComponent);
 		component = fixture.componentInstance;
-		component.summary = summary;
 		fixture.detectChanges();
 	});
 
@@ -28,10 +25,34 @@ describe('ZonePreviewComponent', () => {
 		expect(component).toBeTruthy();
 	});
 
-	it('should display zone name',() => {
-		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector('h3').textContent).toContain('somehost.somezone');
+
+	describe('Hanging zone',() => {
+
+		beforeEach( () => {
+			component.summary = new ZoneSummaryReport('hanging','box')
+			fixture.detectChanges();
+		});
+
+		it('should display zone name',() => {
+			const compiled = fixture.debugElement.nativeElement;
+			expect(compiled.querySelector('h3').textContent).toContain('hanging.box');
+		});
+
+		it('should display a tracking icon', () => {
+			const compiled = fixture.debugElement.nativeElement;
+			expect(compiled.querySelector('svg.card-img-top')).toBeTruthy();
+			expect(compiled.querySelector('img.card-img-top')).toBeFalsy();
+		});
+
+		it('should display no current state', () => {
+			const compiled = fixture.debugElement.nativeElement;
+			let res = compiled.querySelector('li')
+			expect(res.textContent).toBe('No current state');
+
+		});
 
 	});
+
+
 
 });
