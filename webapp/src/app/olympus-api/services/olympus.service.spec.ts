4a9b02cf0b524075c5c9ab6e134c647abf982ff5
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-05-15 22:54:26 +0200

    adds OlympusService.getZoneReportSummaries()

--- webapp/src/app/olympus-api/services/olympus.service.spec.ts
+++ webapp/src/app/olympus-api/services/olympus.service.spec.ts
@@ -1,16 +1,38 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { OlympusService } from './olympus.service';
 
+import fakeDB from '../fake-backend/db.json';
+
 describe('OlympusService', () => {
+  let httpMock: HttpTestingController;
   let service: OlympusService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [OlympusService],
+    });
     service = TestBed.inject(OlympusService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getZoneReportSummaries', () => {
+    it('should call the right endpoint', () => {
+      service.getZoneReportSummaries().subscribe((reports) => {
+        expect(reports.length).toBe(3);
+      });
+      const req = httpMock.expectOne('/api/zones');
+      expect(req.request.method).toBe('GET');
+      req.flush(fakeDB._api_zones);
+    });
   });
 });
--- webapp/src/app/olympus-api/services/olympus.service.ts
+++ webapp/src/app/olympus-api/services/olympus.service.ts
@@ -1,10 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ZoneReportSummary } from '../zone-report-summary';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-
-
 export class OlympusService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  getZoneReportSummaries(): Observable<ZoneReportSummary[]> {
+    return this.httpClient
+      .get<any[]>('/api/zones')
+      .pipe(
+        map((list) => list.map((item) => ZoneReportSummary.fromPlain(item)))
+      );
+  }
 }
