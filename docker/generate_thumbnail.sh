f5942601e2f66b1e76f03da74d7930615672deda
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-06-27 12:08:06 +0200

    Switches to mediamtx instead of nginx-rtmp
    Image is smaller.
    Image needs 0 configuration to work out of the box
    Thumbnail generation is done through a basic script.

---
+++ docker/generate_thumbnail.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+MTX_PATH=$1
+
+
+
+DIR=$(/usr/bin/dirname $1)
+
+mkdir -p /srv/thumbnails/$DIR
+
+while true
+do
+	ffmpeg -i rtmp://localhost/$MTX_PATH -frames:v 1 /srv/thumbnails/$MTX_PATH.jpg 2>/dev/null
+	/bin/sleep 600
+done
